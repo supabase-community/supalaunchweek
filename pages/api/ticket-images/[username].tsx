39d5d7953a0cb8a747e389de87af9d1380540eef
thorwebdev <thor@supabase.io>
2022-07-13 15:24:09 +0800

    Revert "fix: reduce dependencies for ticketImages."
    This reverts commit e83cfa65b8e48f6502b12673f2d47da994dfe68e.

--- pages/api/ticket-images/[username].tsx
+++ pages/api/ticket-images/[username].tsx
@@ -15,14 +15,35 @@
  */
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import screenshot from '@lib/screenshot';
+import { SITE_URL, SAMPLE_TICKET_NUMBER } from '@lib/constants';
 import { getUserByUsername } from '@lib/db-api';
 
-export default async function users(req: NextApiRequest, res: NextApiResponse) {
+export default async function ticketImages(req: NextApiRequest, res: NextApiResponse) {
+  let url: string;
+  let name: string | null | undefined;
+  let ticketNumber: number | null | undefined = SAMPLE_TICKET_NUMBER;
   const { username } = req.query || {};
   if (username) {
     const usernameString = username.toString();
     const user = await getUserByUsername(usernameString);
-    res.json(user);
+    name = user.name;
+    ticketNumber = user.ticketNumber;
+    url = `${SITE_URL}/ticket-image?username=${encodeURIComponent(
+      usernameString
+    )}&ticketNumber=${encodeURIComponent(ticketNumber ?? SAMPLE_TICKET_NUMBER)}`;
+    if (name) {
+      url = `${url}&name=${encodeURIComponent(name)}`;
+    }
+
+    const file = await screenshot(url);
+    res.setHeader('Content-Type', `image/png`);
+    res.setHeader(
+      'Cache-Control',
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    );
+    res.statusCode = 200;
+    res.end(file);
   } else {
     res.status(404).send('Not Found');
   }
